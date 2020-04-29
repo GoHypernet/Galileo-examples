eb78e0d977a64a1da949b54775c24078b79bbaf7
TtheBC01 <todd@hyperdyne.io>
2020-04-28 18:36:47 -0700

    added mpirun

--- ambertools19_example/run.sh
+++ ambertools19_example/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sander -i md.in -c dhfr.crd -p dhfr.prmtop -o dhfr.md.out -r dhfr.md.rst -inf dhfr.md.inf -x dhfr.md.nc -O
+mpirun -np 4 sander.MPI -i md.in -c dhfr.crd -p dhfr.prmtop -o dhfr.md.out -r dhfr.md.rst -inf dhfr.md.inf -x dhfr.md.nc -O
