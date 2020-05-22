e07d62d92054080286c581b9a42e3403f90e6fbd
TtheBC01 <todd@hypernetlabs.io>
2020-05-22 07:19:13 +0000

    update to .bat file

--- TUFLOW_example/runs/_ run all simulations.bat
+++ TUFLOW_example/runs/_ run all simulations.bat
@@ -1,5 +1,6 @@
-Set EXE_iSP=..\exe\2018-03-AC\TUFLOW_iSP_w64.exe
-Set EXE_iDP=..\exe\2018-03-AC\TUFLOW_iDP_w64.exe
+REM - The simulator executable path is preset in the container environment 
+rem Set EXE_iSP=..\exe\2018-03-AC\TUFLOW_iSP_w64.exe
+rem Set EXE_iDP=..\exe\2018-03-AC\TUFLOW_iDP_w64.exe
 REM - Run all example models
 
 start "TUFLOW" /wait /low %EXE_iSP% -b  -sl 5m -s2 EXG -el Q100 -e2 2hr		EG05_2D_~s1~_~s2~_~e1~_~e2~_006.tcf
