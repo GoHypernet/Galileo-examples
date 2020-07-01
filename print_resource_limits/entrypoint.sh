320fea817b59d0f9fcff717c4c6eef8cfeaffd7b
Conrad Bailey <conradbailey92@gmail.com>
2020-07-01 14:09:39 -0700

    memory issue fix in print_resource_limits

--- print_resource_limits/entrypoint.sh
+++ print_resource_limits/entrypoint.sh
@@ -2,5 +2,7 @@
 
 NPROC=$(nproc)
 echo "Number of Processors: $NPROC"
-FREE=$(free -h | tail -n2 | head -n1 | awk '{print $2}')
-echo "Total Memory: $FREE"
+MEMORY=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
+MEMORY=$(python -c "print('%.3fGB' % (int($MEMORY) / 1e9))")
+
+echo "Total Memory: $MEMORY"
