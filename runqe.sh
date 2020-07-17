e21f5186bceabe1b52a6f43b6d7722fae1bea7b0
Todd Chapman <todd@hypernetlabs.io>
2020-07-17 11:16:09 -0700

    Update runqe.sh

--- runqe.sh
+++ runqe.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mpirun -np 1 pw.x < pw.methane.in
-mpirun -np 1 turbo_davidson.x < turbo_davidson.methane.in 
-mpirun -np 1 turbo_spectrum.x < turbo_spectrum.methane.in
+mpirun -np $(nproc) pw.x < pw.methane.in
+mpirun -np $(nproc) turbo_davidson.x < turbo_davidson.methane.in 
+mpirun -np $(nproc) turbo_spectrum.x < turbo_spectrum.methane.in
